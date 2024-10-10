b3ab7b4209f40918fbc822879e9245991113450f
ParitoshAnilNeelamChaurasia <149140841+ParitoshAnilNeelamChaurasia@users.noreply.github.com>
2024-10-11 02:55:06 +0530

    Create 5_1_Merge two sorted linked lists.cpp

---
+++ 5_1_Merge two sorted linked lists.cpp
@@ -0,0 +1,65 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // Check if either list is initially null
+        if (list1 == nullptr) return list2;
+        if (list2 == nullptr) return list1;
+
+        // Initialize the first node based on the smaller value
+        ListNode* first = nullptr; // This will track the head of the merged list
+        if (list1->val <= list2->val) 
+        {
+            first = list1;
+            list1 = list1->next;
+        } 
+        else 
+        {
+            first = list2;
+            list2 = list2->next;
+        }
+
+        // Keep track of the current node for appending
+        ListNode* current = first;
+
+        // Merge the two lists while both are non-null
+        while (list1 != nullptr && list2 != nullptr) 
+        {
+            if (list1->val <= list2->val) 
+            {
+                current->next = list1;
+                list1 = list1->next;
+            } 
+            else 
+            {
+                current->next = list2;
+                list2 = list2->next;
+            }
+            current = current->next; // Move current pointer forward
+        }
+
+        // Attach the remaining nodes if one list is longer than the other
+        if (list1 != nullptr) 
+        {
+            current->next = list1;
+        } 
+        else if (list2 != nullptr) 
+        {
+            current->next = list2;
+        }
+
+        // Return the head of the merged list
+        return first;
+    }
+};
